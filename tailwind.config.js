f0d49b410fafbf841c0029ba695969e3d02771d5
Abhinav Manoj <abhinavmanoj99@gmail.com>
2023-10-07 00:56:52 -0700

    Theme_Changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    colors: {
+      background: "#061F0B",
+      white: "#ffffff",
+      black: "#000000",
+      title: "#60D688",
+      button: "#3B8950",
+      aboutme: "#3B8950",
+      aboutme2: "#1E2A24",
+      projects: "#9E283D",
+      projects2: "#2A1E20",
+      contactme: "#663AC2",
+      contactme2: "#1C1627",
+      titlePrimary: "#E7E7E7",
+      lightGray: "#D3D3D3",
+      darkGray: "#A9A9A9",
+    },
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
